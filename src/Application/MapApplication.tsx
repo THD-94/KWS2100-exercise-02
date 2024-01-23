42de6e6fe90b8ee2a7ed327f1744a8c835f4664d
trinedavidsen <trda007@student.kristiania.no>
2024-01-23 15:51:46 +0100

    Adding OpenLayers

--- src/Application/MapApplication.tsx
+++ src/Application/MapApplication.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {MutableRefObject, useEffect} from "react";
 import "./MapApplication.css"
+import {Map} from "ol";
+import {useRef} from "react/index";
+
+const map = new Map()
 function MapAppliction() {
+    const mapref = useRef() as MutableRefObject<HTMLAnchorElement>
+
+    useEffect(()=>{
+        map.setTarget(mapref.current)
+    }, [])
     return <>
         <header>
             <h1>Map Application</h1>
         </header>
         <nav>Actions</nav>
-        <main>Here is the map</main>
+        <main ref={mapref}>Here is the map</main>
     </>
 }
 export default MapAppliction;
