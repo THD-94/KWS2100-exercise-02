dcaa8bca1f717b143955d24ca993bdea02b60c00
trinedavidsen <trda007@student.kristiania.no>
2024-01-23 16:43:29 +0100

    fixing prettier errors

--- src/Application/MapApplication.tsx
+++ src/Application/MapApplication.tsx
@@ -1,34 +1,39 @@
-import React, {MutableRefObject, useRef, useEffect} from "react";
-import "./MapApplication.css"
-import {Map, View} from "ol";
+import React, { MutableRefObject, useRef, useEffect } from "react";
+import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
-import {OSM} from "ol/source";
-import {useGeographic} from "ol/proj";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
 
-useGeographic()
+import "./MapApplication.css";
+import "ol/ol.css";
+
+useGeographic();
 
 const map = new Map({
-    layers: [
-        new TileLayer({
-            source: new OSM()
-        })
-    ],
-    view: new View({
-        center: [10, 59], zoom: 10
-    })
-})
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: [10, 59],
+    zoom: 10,
+  }),
+});
 function MapApplication() {
-    const mapRef = useRef() as MutableRefObject<HTMLAnchorElement>
+  const mapRef = useRef() as MutableRefObject<HTMLAnchorElement>;
 
-    useEffect(()=>{
-        map.setTarget(mapRef.current)
-    }, [])
-    return <>
-        <header>
-            <h1>Map Application</h1>
-        </header>
-        <nav>Actions</nav>
-        <main ref={mapRef}>Here is the map</main>
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, []);
+  return (
+    <>
+      <header>
+        <h1>Map Application</h1>
+      </header>
+      <nav>Actions</nav>
+      <main ref={mapRef}></main>
     </>
+  );
 }
 export default MapApplication;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 import MapApplication from "./Application/MapApplication";
 
-const root = ReactDOM.createRoot(document.getElementById("root")!)
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 
-root.render(<MapApplication />)
+root.render(<MapApplication />);
