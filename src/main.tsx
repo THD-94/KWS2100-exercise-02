e440241c29bbf131c667904a2d24851167569749
trinedavidsen <trda007@student.kristiania.no>
2024-01-23 15:23:21 +0100

    config typescript

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 const root = ReactDOM.createRoot(document.getElementById("root")!)
-root.render(<h1>Hello React</h1>)
+
+function MapAppliction() {
+    return <>
+        <header>
+            <h1>Map Application</h1>
+        </header>
+        <nav>Actions</nav>
+        <main>Here is the map</main>
+        </>
+}
+
+root.render(<MapAppliction />)
